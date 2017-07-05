b6970fd7c44afcabc60d8a459281c5f818288fde
twotwo <twotwo.li@gmail.com>
2017-07-05 19:31:38 +0800

    新建基础类。目前整合了xcodebuild打ipa的逻辑，本地测试通过

---
+++ util.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+apk自动打包命令行工具
+
+@liyan
+"""
+
+import argparse
+import codecs, os, sys
+from subprocess import Popen, PIPE
+
+import time #计算命令执行时长
+import logging
+
+import platform
+
+
+class Command(object):
+	# config a log
+	logging.basicConfig(filename='./excute.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+	logging.info('launch on %s'%platform.system())
+
+	@staticmethod
+	def isMacSystem():
+		return 'Darwin' in platform.system()
+
+	@staticmethod
+	def isLinuxSystem():
+		return 'Linux' in platform.system()
+
+	@staticmethod
+	def isWindowsSystem():
+		return 'Windows' in platform.system()
+
+	@staticmethod
+	def excute(cmd, dry_run=False):
+		start_point = time.time()
+		if dry_run: 
+			logging.warn('dry-run mode on[%s]'%cmd)
+			return (0, cmd, '')
+		else:
+			logging.debug(cmd)
+		process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+		out, err = process.communicate()
+		if len(err) > 0:
+			logging.error('cmd=%s, err=%s' %(cmd, err))
+		return (time.time()-start_point, out, err)
+
+	@staticmethod
+	def git_ver(git_dir):
+		"""pull the latest content and get version
+		git revision cmd: git rev-list --count HEAD
+		"""
+		cmd = 'git -C %s pull' % git_dir
+		(cost, out, err) = Command.excute(cmd)
+		if len(err) > 0:
+			logging.error('excute[%s]: %s' %(cmd, err))
+		cmd = 'git -C %s rev-list --count HEAD' % git_dir
+		(cost, out, err) = Command.excute(cmd)
+		if len(err) > 0:
+			logging.error('excute[%s]: %s' %(cmd, err))
+		if len(out) > 0:
+			return out.strip('\n')
+		else:
+			return ''
+
+	@staticmethod
+	def svn_ver(svn_dir):
+		"""pull the latest content and get version
+		svn info <path> | grep "Last Changed Rev" | cut -d" " -f4
+		svn info --show-item last-changed-revision <path>
+		"""
+		cmd = 'svn up %s' % svn_dir
+		(cost, out, err) = Command.excute(cmd)
+		if len(err) > 0:
+			logging.error('excute[%s]: %s' %(cmd, err))
+		cmd = 'svn info --show-item last-changed-revision %s' % svn_dir
+		(cost, out, err) = Command.excute(cmd)
+		if len(err) > 0:
+			logging.error('excute[%s]: %s' %(cmd, err))
+		if len(out) > 0:
+			return out.strip('\n')
+		else:
+			return ''
+
+	@staticmethod
+	def xcodebuild_ipa(project, scheme, export, is_release=True, is_xcpretty=True):
+		"""
+		"""
+		build = 'Debug'
+		if is_release:
+			build = 'Release'
+
+		xcpretty = ''
+		if is_xcpretty:
+			xcpretty = '| xcpretty'
+		
+		archive='build/demo.xcarchive'
+		
+		archive_cmd='xcodebuild archive -project %(project_name)s -scheme %(scheme_name)s -configuration %(build_config)s -archivePath %(archive_path)s %(xcpretty)s' % {
+			'project_name': project, 
+			'scheme_name': scheme,
+			'build_config': build,
+			'archive_path': archive,
+			'xcpretty': xcpretty
+		}
+		print archive_cmd
+		(cost, out, err) = Command.excute(archive_cmd)
+		print err
+
+		export_cmd='xcodebuild -exportArchive -archivePath %(archive_path)s -exportPath %(export_path)s -exportOptionsPlist package.plist %(xcpretty)s' % {
+			'export_path': export,
+			'archive_path': archive,
+			'xcpretty': xcpretty
+		}
+		print export_cmd
+		(cost, out, err) = Command.excute(export_cmd)
+		print err
+
+def test():
+	if Command.isMacSystem(): print 'launch on OS X!'
+	if Command.isLinuxSystem(): print 'launch on Linux!'
+	if Command.isWindowsSystem(): print 'launch on Windows!'
+
+	try:
+		print 'current dir git version =', Command.git_ver(".")
+	except:
+		pass
+
+	if len(sys.argv) >1:
+		print 'svn dir =', sys.argv[1], 'version =', Command.svn_ver(sys.argv[1])
+	else:
+		print 'add svn path'
+
+	Command.xcodebuild_ipa(project='FLGamePlatformDemo.xcodeproj', scheme='FLGamePlatformDemoRelease', export='build')
+
+if __name__ == '__main__':
+	test()
