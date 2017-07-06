9d416ce02e08db90ba2ad7216295fa693392946a
twotwo <twotwo.li@gmail.com>
2017-07-06 12:01:42 +0800

    update util.py

--- util.py
+++ util.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-apk自动打包命令行工具
+命令行工具类
 
-@liyan
+Author: liyan
+File: util.py
+
+Features:
+1. 本地命令执行： (cost, out, err) = Command.excute(cmd, dry_run=False)
+2. 系统平台判断： Command.isXXXSystem()
+3. 当前版本判断： Command.git_ver(dir)/Command.svn_ver(dir)
+4. 基于android工具的出apk包的执行命令: Command.
+5. xcode 8.2+出ipa包的执行命令： Command.xcodebuild_ipa(project, scheme, export)
+
 """
 
 import argparse
@@ -84,8 +93,27 @@ class Command(object):
 			return ''
 
 	@staticmethod
-	def xcodebuild_ipa(project, scheme, export, is_release=True, is_xcpretty=True):
+	def apk(project, target='android-21', is_release=True):
+		"""基于android工具的出apk包的执行命令:
+		project: 工程所在路径
+		target: Target ID to set for the project
 		"""
+		build = 'debug'
+		if is_release:
+			build = 'release'
+		update_project_cmd = 'android update project -l ../%s -p  %(project_dir)s -n MainActivity -t %(target_id)s' % {
+			'project_dir': project, 
+			'target_id': target
+		}
+		ant_cmd = 'ant -f %(project_dir)s/build.xml clean %(project_dir)s'% {
+			'project_dir': project, 
+			'build_config': build,
+			'xcpretty': xcpretty
+		}
+
+	@staticmethod
+	def xcodebuild_ipa(project, scheme, export, is_release=True, is_xcpretty=True):
+		"""xcode 8.2+出ipa包的执行命令
 		"""
 		build = 'Debug'
 		if is_release:
@@ -131,8 +159,6 @@ def test():
 		print 'svn dir =', sys.argv[1], 'version =', Command.svn_ver(sys.argv[1])
 	else:
 		print 'add svn path'
-
-	Command.xcodebuild_ipa(project='FLGamePlatformDemo.xcodeproj', scheme='FLGamePlatformDemoRelease', export='build')
 
 if __name__ == '__main__':
 	test()
