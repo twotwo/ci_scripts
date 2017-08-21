82c44ee177f8f219057af2c8051a1d2c59adebd1
twotwo <twotwo.li@gmail.com>
2017-08-21 18:36:02 +0800

    line 110: add missing mv command

--- apk_builder.py
+++ apk_builder.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 """
 apk自动打包命令行工具
 
@@ -6,159 +6,135 @@ Author: liyan
 File: apk_builder.py
 
 Features:
-1. build_agent_demos(): build my multiple module project
-2. 
+1. merge_plugin_and_build_apk(): merge my plugin to project and build to apk
+2. build_agent_demos(): build my multiple module project use AgentBuilder class
 """
 
-from util import Command
+from util import Command, AgentBuilder
 
-import argparse, logging
-import os, ConfigParser
-
-class AgentBuilder(object):
-	def __init__(self, ini_file, dry_run=False):
-		self.dry_run = dry_run
-
-		# config = ConfigParser.RawConfigParser(allow_no_value=True)
-		config = ConfigParser.ConfigParser()
-		config.read(ini_file)
-		root_dir = config.get('base', 'root_dir')
-
-		self.lib_base_dir = config.get('base', 'lib_base_dir', 0, {'root_dir': root_dir})
-		self.channels_dir = config.get('base', 'channels_dir', 0, {'root_dir': root_dir})
-
-		self.demo_dir = config.get('demo', 'demo_dir', 0, {'root_dir': root_dir})
-		from datetime import date
-		self.apk_dir = config.get('demo', 'apk_dir', 0, {'root_dir': root_dir, 'day':date.today().strftime('%m%d')})
-		
-		self.plugin_dir = config.get('plugins', 'plugin_dir', 0, {'root_dir': root_dir})
-
-	def init(self):
-		# init output dirs
-		if os.path.exists(self.demo_dir):
-			Command.excute('rm -rf %s' % self.demo_dir)
-		os.makedirs(self.demo_dir)
-		if not os.path.exists(self.apk_dir):
-			os.makedirs(self.apk_dir)
-
-	def build_baselib(self):
-		# cp -R lib_base_dir demo_dir/lib_base
-		Command.excute('cp -R %s %s/lib_base'%(self.lib_base_dir, self.demo_dir) )
-
-		# android update & ant debug
-		Command.excute('android update project -p %s/lib_base -n MainActivity -t android-21' % self.demo_dir)
-		Command.excute('ant -f %s/lib_base/build.xml clean debug' % self.demo_dir)
-
-		# cp demo_dir/lib_base/bin/classes.jar demo_dir/plugin_base-revision.jar
-		revision = Command.svn_ver(self.lib_base_dir)
-		base_jar = os.path.join(self.demo_dir, 'plugin_base-r%s.jar' % revision)
-		from_jar = os.path.join(self.demo_dir, 'lib_base/bin/classes.jar')
-		cmd = 'cp %s %s'% (from_jar, base_jar)
-
-		print '[%s] create plugin base lib. %s %s'%Command.excute(cmd)
-
-	def build_channel_apks(self, name):
-		"""build channel apks to apk_dir
-		name: all or channel name in project name
-		"""
-		# project name: FLSDK_channel_<channel>_combine
-		for project in os.listdir(self.channels_dir):
-			if not os.path.isdir( os.path.join(self.channels_dir, project) ): continue
-			if project.find('.') == 0: continue
-
-			# verify channel name
-			if len(project.split('_'))!=4:
-				print 'unknown project[%s]!'%project
-				logging.error( 'unknown project[%s]!'%project )
-				continue
-			channel = project.split('_')[2]
-
-			# 3.2 build all or certain channel
-			if 'all' == name or channel == name:
-				self.__build_lib_and_demoapk(project, channel, 'lib_base')
-		
-	def __build_lib_and_demoapk(self, project, channel, library):
-		"""给svn中最新的适配代码打包，生成插件类库和demo apk
-		project 	: project under demo;
-		channel 	: channel name in project;
-		library     : Directory of an Android library to add;
-		"""
-		code_dir = os.path.join(self.channels_dir, project)
-		sa_dir = os.path.join(self.demo_dir, project)
-
-		if os.path.exists(sa_dir) :
-			Command.excute('rm -rf %s'%sa_dir )
-		Command.excute('cp -R %s %s'%(code_dir, self.demo_dir), self.dry_run )
-
-
-		game=os.environ.get('game')
-		channel_ver = 'unknown'
-		if os.path.exists( os.path.join(sa_dir,'SDKVersion.txt') ) :
-			channel_ver = open(os.path.join(sa_dir,'SDKVersion.txt')).readline().strip()
-
-		logging.info( 'demo_dir=%s, game=%s, channel=%s, ver=%s'%(self.demo_dir, game, channel, channel_ver) )
-		
-		# os.chdir(sa_dir)
-		logging.info('===== [%s] Build Start: %s ' % (project, os.getcwd()) )
-
-		revision = Command.svn_ver(code_dir)
-		print 'build %s@r%s' % (sa_dir, revision)
-
-		print 'create android project ... '
-		cmd = 'android update project -p %s -n MainActivity -t android-21'%sa_dir
-		if library:
-			print Command.excute('cp ant.properties %s'%sa_dir)
-			print Command.excute('rm %s/project.properties'%sa_dir)
-			cmd = 'android update project -l ../%s -p %s -n MainActivity -t android-21' % (library, sa_dir)
-			# f = open('project.properties', 'w') #'r', 'w' or 'a'
-			# f.write(project_properties)
-			# f.close
-		 # -t --target     : Target ID to set for the project.
-		 # android-21 Name: Android 5.0.1
-		 # android-22 Name: Android 5.1.1
-		build='debug'
-		if os.path.exists( os.path.join(sa_dir,'%s.properties'%game) ) :
-			print Command.excute('mv %s %s'%(os.path.join(sa_dir,'%s.properties'%game), os.path.join(sa_dir,'ant.properties')))
-			build='release'
-		(cost, out, err) = Command.excute(cmd)
-		if len(err) > 0: 
-			status = '[failed]'
-			print status, cmd , err
-		else:
-			status = '[ok]'
-			print status, cmd
-
-		print 'build android project ... '
-		cmd = 'ant -f %s/build.xml clean %s'%(sa_dir,build)
-		(cost, out, err) = Command.excute(cmd, self.dry_run)
-		if len(err) > 0: 
-			status = '[failed]'
-			print status, cmd , err
-		else:
-			status = '[ok]'
-			print status, cmd
-
-		if '[ok]' == status and library:
-			# build apk, such as 360_c1.9.2_r168988_b62.apk
-			version_info= 'c%s_r%s_b%s' % (channel_ver, revision, os.environ.get('BUILD_NUMBER'))
-			(cost, out, err) = Command.excute('cp %(project_dir)s/bin/MainActivity-%(build_type)s.apk %(dist_dir)s/%(apk_name)s.apk' % {
-				'project_dir': sa_dir,
-				'dist_dir': self.apk_dir,
-				'apk_name': channel+'\('+build+'\)_'+version_info,
-				'build_type': build
-				})
-			if len(err) > 0: print err
-
-			# build plugin lib ,such as plugin_360_c1.9.2_r168988_b62.jar
-			if not os.path.exists( os.path.join(sa_dir,'plugin_task.xml') ):
-				Command.excute('cp plugin_task.xml %s'% sa_dir) 
-			Command.excute('ant -f %s/plugin_task.xml -Drevision=%s -Dchannel=%s'% (sa_dir, version_info, channel ))
-
-		logging.info('===== [%s] Build Over %s -r%s' % (project, status, revision) )
+import argparse
+import logging
+import os, sys
 
 def test():
 	
 	print 'pwd=%s' % os.getcwd()
+
+def merge_plugin_and_build_apk(args):
+	"""Excute Help: python apk_builder.py -c apk -s /data/game_apks/g103_3 -ch qh
+-c command type, must be apk
+-s read source code from this directory
+-ch witch channel plugin merge to source
+lftp -c 'open -e "pget -n 4 games/qzgs_201_08211523.rar" sdk:123456@172.16.100.120'
+wget https://github.com/dryes/rarlinux/raw/master/rarlinux-5.2.1.tar.gz
+
+操作逻辑说明： 
+1. init: 初始化工程目录，删除存在的工程目录
+2. copy: 复制项目代码到工程目录
+3. clean: 清理工程目录中的默认插件代码
+4. merge: 把指定插件拷贝到工程目录
+5. update: 更新versionName和versionCode
+6. build: 打包
+7. save: 拷贝到指定目录
+
+- 工作目录
+1. sdk-u3d-plugins：根据apk_builder.py路径，从而确定plugins路径；
+2. 工程目录(apk_dir, 执行ant release的目录)：当前执行命令的路径下的build_<plugin name>目录，即 os.getcwd()/build_<plugin name>
+
+
+- 渠道打包命名规则
+应用名称_渠道编码-应用目录名-插件版本号.apk
+
+应用举例：g20_exit/g20_noexit
+插件举例：p23 - 对应的git revision
+	"""
+	status = 'init'
+
+	game_dir = args.src
+	channel = args.channel
+
+	apk_dir = os.path.join(os.getcwd(),'build_%s'%channel)
+	plugin_dir = sys.path[0]
+	logging.info( '[%s]plugin_dir = %s, apk_dir = %s' % (status, plugin_dir, apk_dir) ) 
+
+	revision = Command.git_ver(plugin_dir)
+
+	if len(revision) == 0:
+		print 'revision missing'
+		logging.warn('revision missing')
+
+	if os.path.exists(apk_dir):
+		print 'apk_dir exists, you should delete before build.'
+		return
+
+	apk_name = 	channel +'-'+ os.path.basename(game_dir)
+	
+	# 拷贝母包
+	status = 'copy'
+	(cost, out, err) = Command.excute('cp -R %s %s' % (game_dir, apk_dir), args.dry_run)
+	logging.info( '[%s]from_dir = %s, to_dir = %s' % (status, game_dir, apk_dir) ) 
+
+	# 母包初始化
+	clean_and_init_project(apk_dir)
+	# cp channel keystore & ant.properties
+	keystore = os.path.join(plugin_dir,'keys.%s/%s.keystore'%(args.app,channel))
+	ant_p = os.path.join(plugin_dir,'keys.%s/ant_%s.properties'%(args.app,channel))
+	if os.path.exists(keystore) and os.path.exists(ant_p):
+		(cost, out, err) = Command.excute('cp %s %s' % (keystore, apk_dir), args.dry_run)
+		print Command.excute('cp %s %s/ant.properties' % (ant_p, apk_dir), args.dry_run)
+
+	(cost, out, err) = Command.excute('cp -Rv %s/plugin_%s/* %s' % (plugin_dir, channel, apk_dir), args.dry_run)
+	# change versionCode&versionName in AndroidManifest.xml
+	cmd = '''sed -i.bak -r '{s/android:versionCode=\s*\"[0-9]{1,}\"/android:versionCode=\"%s\"/g;s/android:versionName=\s*\"[^"]+\"/android:versionName=\"%s\"/g}' %s/AndroidManifest.xml''' % (args.versioncode, args.versionname, apk_dir)
+	(cost, out, err) = Command.excute(cmd)
+	print cmd
+
+	print Command.excute('android update project -p %s -n %s -t %s' % (apk_dir, apk_name, args.target), args.dry_run)
+	(cost, out, err) = Command.excute('ant -f %s/build.xml clean release' % apk_dir, args.dry_run)
+	status = 'build'
+	logging.info('[%s] Build Game Package, channel=%s, err=%s' % (status, channel, err) )
+	
+	apk_to = apk_name+'-p'+revision+'-release_vc'+args.versioncode+ '.apk'
+
+	status = 'save'
+	cmd_cp_apk = 'cp %s/bin/%s %s'%(apk_dir, apk_name+'-release.apk', apk_to)
+	(cost, out, err) = Command.excute(cmd_cp_apk, args.dry_run)
+	logging.info('[%s] Save Game Package, cmd=%s, err=%s' % (status, cmd_cp_apk, err) )
+
+	status = 'save with build number'
+	if None != os.environ.get('BUILD_NUMBER'):
+		apk_to = os.path.join(os.environ.get('WORKSPACE'),'game_apks',os.environ.get('BUILD_NUMBER'))
+		if not os.path.exists(apk_to):
+			os.mkdir(apk_to)
+		cmd_mv_apk = 'mv %s %s'%(apk_name+'-release.apk', apk_to)
+		(cost, out, err) = Command.excute(cmd_mv_apk, args.dry_run)
+		logging.info('[%s] Move Game Package, cmd=%s, err=%s' % (status, cmd_mv_apk, err) )
+
+
+def clean_and_init_project(app_dir = 'game'):
+	"""母包初始化，拷贝到合成目录后：先清理特定渠道内容、再放入必要参数
+	"""
+	app_name='qzgs'
+
+	plugin_dir = sys.path[0]
+
+	# 清理
+	# libs 7 packages
+	Command.excute('rm %s/libs/alipaySdk-*.jar'%app_dir)
+	Command.excute('rm -rf %s/libs/CommonChannel*.jar'%app_dir)
+	Command.excute('rm %s/libs/plugin_base*.jar'%app_dir)
+	Command.excute('rm %s/libs/plugin_feiliu*.jar'%app_dir)
+	Command.excute('rm %s/libs/android-support-v4.jar'%app_dir)
+	Command.excute('rm %s/libs/FL*.jar'%app_dir) #2 packages
+	Command.excute('rm %s/libs/ok*.jar'%app_dir) #2 packages
+	Command.excute('rm %s/libs/gson*.jar'%app_dir)
+	Command.excute('rm %s/AndroidManifest.xml'%app_dir)
+
+	# 添加
+	keystore = os.path.join(plugin_dir,'keys.%s/AndroidFLMobile_v2.keystore'%app_name)
+	ant_p = os.path.join(plugin_dir,'keys.%s/ant.properties'%app_name)
+	Command.excute('cp %s %s' % (keystore, app_dir))
+	Command.excute('cp %s %s' % (ant_p, app_dir))
 
 def build_agent_demos(args):
 	"""sdk-agent适配各个渠道的demo，一锅出多个Android apk和适配层类库
@@ -170,19 +146,8 @@ def build_agent_demos(args):
 	builder.build_baselib()
 
 	builder.build_channel_apks(args.channel)
-		
 
-def build_apk(args):
-	"""clean project, add plugin -> build to apk : 
-	python apk_builder.py -c apk -s ../games/qz20_exit -ch uc
-
-- 渠道打包命名规则
-应用名称_渠道编码-应用版本好-插件版本号.apk
-
-应用举例：app_dir
-插件举例：p23 - 对应的git revision
-	"""
-
+	logging.info('==== Build %d channels ====\n'%len(builder.build_info)+'\n'.join(builder.build_info))
 
 def main():
 	parser = argparse.ArgumentParser(description='APK Builder.')
@@ -196,24 +161,27 @@ def main():
 											help='Build command')
 	parser.add_argument('-ch', dest='channel', type=str, default='all',
 											help='Channel Name, all or a certain name')
+	parser.add_argument('-vn', dest='versionname', type=str, default='1.0.0', 
+											help='Android manifest versionName')
+	parser.add_argument('-vc', dest='versioncode', type=str, default='1', 
+											help='Android manifest versionCode')
 	parser.add_argument('--dry-run', dest='dry_run', action='store_true',
 											help='Dry Run Mode: do not excute time-consuming operation')
 	parser.set_defaults(dry_run=False)
-	# config a log
-	logging.basicConfig(filename='./build.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 	args = parser.parse_args()
 
 	if args.dry_run: print 'in dry-run mode'
 		
-	if 'demo' == args.cmd:
+	if 'apk' == args.cmd:
+		merge_plugin_and_build_apk(args)
+	elif 'demo' == args.cmd:
 		build_agent_demos(args)
-	elif 'apk' == args.cmd:
-		build_apks(args)
 	elif 'help' == args.cmd:
 		parser.print_help()
 
 	# test()
 
 if __name__ == '__main__':
+	logging.basicConfig(filename='./build.log', level=logging.INFO, format='%(asctime)s [%(name)s] %(levelname)s %(message)s')
 	main()
